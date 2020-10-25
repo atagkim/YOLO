976ec792b8c03ccb2698313df0a40caa845f4999
OhGyuseok <rbtjr970525@gmail.com>
2020-10-25 19:42:43 +0900

    Add Help

--- UntackedVirtualBlackBoard/modules/helper.py
+++ UntackedVirtualBlackBoard/modules/helper.py
@@ -1,34 +1,7 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("Helper")
-        MainWindow.resize(1000, 445)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setObjectName("label")
-        self.label.setPixmap(QtGui.QPixmap("images/INFO.jpg")) #image path
-
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("Helper", "Helper"))
+import cv2
 
 def helper_func():
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-
-    sys.exit(app.exec_())
-
+    image = cv2.imread("images/INFO.jpg", cv2.IMREAD_ANYCOLOR)
+    cv2.imshow("Helper", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
